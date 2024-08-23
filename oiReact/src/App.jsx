bfddb29e2701fc39fde4f570decaa84129a0f6ed
LucianoCoimbraJ <lucianojuniorph@gmail.com>
2024-08-23 17:16:13 -0300

    Criação do primeiro projeto com React bonitão

---
+++ oiReact/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+         <h1>Dinossauros</h1>
+         <h2>Carnívoros</h2>
+         <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quo labore dignissimos pariatur 
+          necessitatibus quam ut? Odit quas tempora doloremque, adipisci provident cumque quibusdam 
+          aliquam voluptate, amet id, sint magnam accusamus?</p>
+    </>
+  )
+}
+
+export default App
