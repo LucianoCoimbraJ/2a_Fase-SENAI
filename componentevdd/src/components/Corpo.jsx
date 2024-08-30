e00dd557150f96a00c53f168640e24f5af254941
LUCIANO COIMBRA DE ALMEIDA JUNIOR <luciano_almeida-j@estudante.sesisenai.org.br>
2024-08-30 17:08:15 -0300

    Continuação do repositorio

---
+++ componentevdd/src/components/Corpo.jsx
@@ -0,0 +1,11 @@
+
+function Corpo() {
+  return (
+    <div>
+        <h2>Eu sou ocorpinho da página. Sou muito mais que só um corpinho bonito..... </h2>
+        <img src="./images/imagem-meme.png" alt=""/>
+    </div>
+  )
+}
+
+export default Corpo
